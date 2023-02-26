6707a1a16f86524444ea8ed62d36c0d4fd271c2e
RandomUser <mad.aalachi@gmail.com>
2023-02-26 02:31:41 +0100

    code update

--- CleanData.py
+++ CleanData.py
@@ -1,11 +1,12 @@
 import os 
 
-
+Data = ''
 files = os.listdir('data')
 for a in files[:10] :
     if '.txt' in a :
         f = open('./data/'+a,'r')
-        Data = f.read()
+        ata = f.read()
+        Data += ata
         f.close()
 
 Data = Data.split("------------------------------")
@@ -15,29 +16,32 @@ Tada = []
 for a in Data :
     Tada.append(list(a.split('\n')))
 
-for a in Tada : 
+for k in range(len(Tada)) : 
     j=0
     i=0
-    while (i-j<len(a)) :
-        if a[i-j] == '' :
-            a.pop(i-j)
+    while (i-j<len(Tada[k])) :
+        if Tada[k][i-j] == '' :
+            Tada[k].pop(i-j)
             j+=1
-        elif 'Afficher cette' in a[i-j] :
-            a.pop(i-j)
+        elif 'Afficher cette' in Tada[k][i-j] :
+            Tada[k].pop(i-j)
             j+=1
-        elif a[i-j] == '·' :
-                a.pop(i-j)
-                j+=1
-
+        elif Tada[k][i-j] == '·' :
+            Tada[k].pop(i-j)
+            j+=1
         i+=1
+    try : 
+        if Tada[k][-4].isnumeric() or (Tada[k][-4].split(' ')[0]+Tada[k][-4].split(' ')[1]).isnumeric() :
+            Tada[k] = Tada[k][:-4]
+    except : 
+        pass
 
 
 for a in Tada :
     try :
-        if not a[-4].isnumeric() :
-            print(a[-4])
+        print(a[-4] + '--' +a[-3] + '--' +a[-2] + '--' +a[-1]  )
     except :
-        print(a)
+        print("HADaaa",a)
 
 
 
