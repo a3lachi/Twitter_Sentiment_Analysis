4ace2985ffaa6a64f7a3c7825671922da948b208
RandomUser <mad.aalachi@gmail.com>
2023-02-27 09:23:35 +0100

    code update

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from Scrap import *
 
 def main():
     # Start_Threads(Number of threads , Headless mode , Swipe Rate ,Folder to save data in ./data folder , Number of trends to scrap )
-    Start_Threads(3,False,20,"26FEB",40)
+    try :
+        Start_Threads(3,False,20,"26FEB",40)
+        print('Threads ended with sucess.')
+    except : 
+        print('Failed launching threads.')
 
 
 
