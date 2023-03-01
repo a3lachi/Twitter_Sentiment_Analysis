3ff1d138d5280bf6f13197883a078c87704e4a98
a3lachi <mad.aalachi@gmail.com>
2023-03-01 21:07:26 +0100

    code update

--- CleanData.py
+++ CleanData.py
@@ -1,4 +1,5 @@
 import os 
+import pandas as pd
 
 
 def CheckNum(strng) :
@@ -10,10 +11,11 @@ def CheckNum(strng) :
     except :
         return False
 
+## Load all data in folder and return it as a string
 def LoadData(Folder) :
     Data = ''
     files = os.listdir('data/'+Folder)
-    for a in files :
+    for a in files[:4] :
         if '.txt' in a :
             f = open('./data/'+Folder+'/'+a,'r')
             ata = f.read()
@@ -24,74 +26,40 @@ def LoadData(Folder) :
 def ProcessData(Data) :
     Data = Data.split("------------------------------")
     Tada = []
+
     for a in Data :
-        Tada.append(list(a.split('\n')))
+        Tada.append(list(a.split('\n'))[1:])
 
-    k=0
-    while  k<len(Tada) :
-        if len(Tada[k])>3 :
-            #Tada[k].pop(2) 
-            j=0
-            i=0
-            while (i-j<len(Tada[k])) :
-                if Tada[k][i-j] in ['Afficher cette discussion' , 'En réponse à ' , '·' , '' ] or Tada[k][i-j][0]=='#'  :
-                    Tada[k].pop(i-j)
-                    j+=1
-                try :
-                    if Tada[k][1][0] == '@' :
-                        Tada[k] = Tada[k][2:]
-                except :
-                    pass
-                
-                i+=1
-
-            try :
-                if CheckNum(Tada[k][-4]) :
-                    Tada[k] = Tada[k][:-4]
-                elif  CheckNum(Tada[k][-3]) :
-                    Tada[k] = Tada[k][:-3]
-                elif  CheckNum(Tada[k][-2]) :
-                    Tada[k] = Tada[k][:-2]
-                elif  CheckNum(Tada[k][-1]) :
-                    Tada[k] = Tada[k][:-1]
-            except :
-                pass
-
-            k+=1
-
-        else :
-            Tada.pop(k)
+    df = pd.DataFrame(columns=['User','Tweet'])
 
 
-    Data = []
+    for i in range(len(Tada)) :
+        if len(Tada[i])>1 and len(Tada[i][1])>1 :
+            pada = pd.DataFrame([Tada[i][1]],columns=['User'])
+            df = pd.concat([df,pada])
+
+            Tada[i]=Tada[i][4:]
+        Tada[i].pop(-1)
+
     for a in Tada :
-        try :
-            if (a[0][0]).isnumeric() :
-                Data.append(a[1:])
-        except :
-            pass
+        if len(a)>0 and a[0]=='En réponse à ' :
+            a.pop(0)
+            a.pop(0)
+        print(a[-4:])
 
-
-    Tada = []
-    for i in range(len(Data)) :
-        if len(Data[i])==2 :
-            Data[i].pop(1)
-        Tada.append(Data[i])
-
-    return Tada
-
+    ## print(df.head(20))
 
 
 Data = LoadData('26FEB')
 
-Tada = ProcessData(Data)
 
-print(len(Data))
-print(len(Tada))
-i=0
-for a in Tada :
-    i+=1*(len(a)==1)
-print(i)
+
+ProcessData(Data)
+
+
+
+    
+
 
 
 
--- test.py
+++ test.py
@@ -1,39 +1,19 @@
-import time 
+def her(nums) :
+    zero , one , two , i = [] , [] , [] , 0
 
-def Dotmatch(s,p) :
-    if (len(s)==len(p)) :
-        for i in range(len(s)) :
-            if s[i]=='.' :
-                return 0
-            elif s[i]!=p[i] :
-                return 1
-        return 2
-    else :
-        return 3 
-
-def isMatch(s, p):
-    print('s --> ' ,s)
-    print('p --> ', p)
-    pi = p.split('*')
-    print('mfrqa --> ',pi)
-    ppi = len(pi)
-    b=0
-    bb=len(pi[b])
-    if(ppi>1) :
-        while (b<ppi and pi[b]!='' and s[:bb]!='') :
-            bb=len(pi[b])
-            print(pi[b],' -- ',s[:bb])
-            zlt = Dotmatch(pi[b],s[:bb])
-            if zlt == 2 :
-                s=s[bb:]
-            elif zlt in [0,1] :
-                b+=1
-
-        return s==''
-    else :
-        if Dotmatch(p,s)==3 :
-            return False 
+    while (i<len(nums)) :
+        print(i)
+        print('Listaa : ',nums)
+        if nums[i]<1 :
+            nums.pop(i)
+            nums.insert(0,0)
+            i+=1
+        elif nums[i]>1 :
+            nums.pop(i)
+            nums.append(2)
+        else :
+            i+=1
 
 
 
-print(isMatch("mississippi",'mis*is*ip*.'))
+her([2,0,2,1,1,0])
