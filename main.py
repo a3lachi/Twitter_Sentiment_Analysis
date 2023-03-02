d2dc7e66d8e3cb091a199929a11f40ff1ba65381
a3lachi <mad.aalachi@gmail.com>
2023-03-02 20:22:11 +0100

    code update

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ from CleanData import *
 def main():
 
 
-    Folder = "/Users/farawa/Twitter_Sentiment_Analysis/data/L2MAR"
+    Folder = "/Users/farawa/Twitter_Sentiment_Analysis/data/26FEB"
     nmbThreads = 2
     headBool = True
     swipeRate = 3
     nmbTrends = 3
 
 
-    Start_Threads(nmbThreads,headBool,swipeRate,Folder,nmbTrends)
+    ##Start_Threads(nmbThreads,headBool,swipeRate,Folder,nmbTrends)
       
 
     DataFrame = ProcessData(Folder)
@@ -20,11 +20,12 @@ def main():
 
 
 
-    print('TOTAL: ',len(DataFrame.index))
-
-    print('')
+    print('Number of tweets : ',len(DataFrame.index),'\n')
 
     print(DataFrame.head(20))
+    print('')
+
+
 
 if __name__ == "__main__":
 
