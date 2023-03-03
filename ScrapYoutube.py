6c8d9ca5b9dd3c3be757e8103c2f13590e9dc712
a3lachi <mad.aalachi@gmail.com>
2023-03-03 02:00:23 +0100

    code update

--- ScrapYoutube.py
+++ ScrapYoutube.py
@@ -11,20 +11,30 @@ import pandas as pd
 
 
 
+def GetTrendingVideos() :
+	options = Options()
+	##options.headless = True
+	driver_trending = webdriver.Firefox(options=options)
 
-options = Options()
-##options.headless = True
-driver_trending = webdriver.Firefox(options=options)
+	driver_trending.get('https://www.youtube.com/feed/trending')
 
-driver_trending.get('https://www.youtube.com/feed/trending')
+	videos = driver_trending.find_element(By.ID,"contents")
 
-videos = driver_trending.find_element(By.ID,"contents")
-
-vidz = videos.find_elements(By.XPATH,"//div[@id='contents']//div[@id='grid-container']//a[@id='video-title']")
-
-
-df = pd.DataFrame(Data,)
+	vidz = videos.find_elements(By.XPATH,"//div[@id='contents']//div[@id='grid-container']//a[@id='video-title']")
 
 
-for video in vidz :
-	a.get_attribute('title')
+
+	Data = []
+
+	for video in vidz :
+		Data.append([video.get_attribute('title'),video.get_attribute('href')])
+
+
+
+
+
+
+	df = pd.DataFrame(Data,columns=['Video','Link'])
+
+
+	print(df.head(10))
