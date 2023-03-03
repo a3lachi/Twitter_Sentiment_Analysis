931328dd47bdc7f7c508c243ba07545aface5014
a3lachi <mad.aalachi@gmail.com>
2023-03-03 01:32:08 +0100

    code update

---
+++ ScrapYoutube.py
@@ -0,0 +1,20 @@
+
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+import time 
+import threading
+import os 
+from selenium.webdriver.common.by import By
+
+
+
+
+
+
+
+
+driver_trending = webdriver.Firefox()
+
+
+driver_trending
+
---
+++ ha.sh
@@ -0,0 +1,8 @@
+
+
+for i in {1..20} :
+do
+	gill ;
+	sleep 100 ;
+
+done
